517291468e311cbb29cc0b797fb7c9a8327ce861
Jameson Andrade <jameson_andrade@hotmail.com.br>
2023-04-13 15:12:59 -0300

    fix show students by registration --fix

--- src/modules/Students/services/showStudentsService.py
+++ src/modules/Students/services/showStudentsService.py
@@ -16,9 +16,14 @@ class ShowStudentsService():
             Registration.student,
             func.max(Registration.year).label('max_year')
         ).group_by(Registration.student).subquery()
+
         results = db.session.query(Student, Registration)\
-        .join(Registration, Student.id == Registration.student)\
-        .join(subquery, and_(Registration.student == subquery.c.student, Registration.year == subquery.c.max_year))\
+        .join(
+            Registration,
+            Student.id == Registration.student)\
+        .join(subquery, and_(
+            Registration.student == subquery.c.student,
+            Registration.year == subquery.c.max_year))\
         .filter(Student.disabled == False)\
         .all()
         student_list = []
